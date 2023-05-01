e2046588100a38366ee03ccfa28ede2c81b1678d
Abir <abirovi481@gmail.com>
2023-05-01 23:49:12 +0600

    second-commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const chefs = require('./data/chefs.json')
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Cheflo is running')
+})
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs)
 })
 
 app.listen(port, () => {
